0b32cb46f1384bde4b1dcf1311edfe08b1f71041
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2024-06-15 09:21:20 +0300

    removed yandex api key

--- src/settings.js
+++ src/settings.js
@@ -3,6 +3,6 @@ export const settings = {
     attempts: 6,
     letterCardSize: 70,
     letterCardSpaces: 3,
-    yandex_api_key: "dict.1.1.20240613T093817Z.de0dba1b6920ae6d.6cd6abb494861361bfca2d91efef8a45d3436e90",
+    yandex_api_key: "",
     words_list_url: "words.txt"//"/sites/default/files/words.txt"//
 }
