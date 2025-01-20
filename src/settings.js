efcdb8183e3afbb335f2d0412dad0902c6bbf068
Arturas-Alfredas Lapinskas <a.lapinskas@noreplay.com>
2025-01-20 10:18:48 +0300

    0.0.8: updated jsge to 1.5.3

--- src/settings.js
+++ src/settings.js
@@ -5,5 +5,5 @@ export const settings = {
     letterCardSpaces: 3,
     shiftSize: 2,
     yandex_api_key: "dict.1.1.20240616T070649Z.78c07cd3af68b4d6.c71c1cb194341c724097b0e1e1656e2a4b37f81f",
-    words_list_url: "/sites/default/files/words.txt"//"words.txt"//
+    words_list_url: "words.txt"//"/sites/default/files/words.txt"//
 }
